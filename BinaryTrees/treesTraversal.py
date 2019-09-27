891da80762edfa322b10f5a28388adbc4ec27d04
postpascal🦉 <apostpascal@gmail.com>
2019-09-27 23:16:44 +0800

    update

--- BinaryTrees/treesTraversal.py
+++ BinaryTrees/treesTraversal.py
@@ -6,6 +6,15 @@ class TreeNode(object):
 		self.left = None
 		self.right = None
 
+def buildtreeslevelOrder(l=1,i=1,n=4):
+	if n==l:
+		return None
+	n=2**l-i
+	node=TreeNode(n)
+	node.left=buildtreeslevelOrder(l+1,2*i)
+	node.right=buildtreeslevelOrder(l+1,2*i-1)
+	return node
+
 def inorderTraversal(root):
 	"""
 	:type root: TreeNode
@@ -29,10 +38,6 @@ def inorderTraversal(root):
 	return res
 
 def preorderTraversal(root):
-	"""
-	:type root: TreeNode
-	:rtype: List[int]
-	"""
 	res = []
 	if not root:
 		return res
@@ -50,6 +55,27 @@ def preorderTraversal(root):
 		
 	return res
 
+# 后序打印二叉树（非递归）
+# 使用两个栈结构
+# 第一个栈进栈顺序：左节点->右节点->跟节点
+# 第一个栈弹出顺序： 跟节点->右节点->左节点(先序遍历栈弹出顺序：跟->左->右)
+# 第二个栈存储为第一个栈的每个弹出依次进栈
+# 最后第二个栈依次出栈
+
+def postOrderTraversal(node):
+    stack = [node]
+    stack2 = []
+    while len(stack) > 0:
+        node = stack.pop()
+        stack2.append(node)
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+    while len(stack2) > 0:
+        print(stack2.pop().val)
+
+
 def levelOrderTraversal(root):
 	d=defaultdict(list)
 	def recursiver(root,i):
@@ -60,16 +86,33 @@ def levelOrderTraversal(root):
 	recursiver(root,0)
 	return d.values()
 
+class BinaryTree(object):
+	def __init__(self, root=None):
+		self.root = root
 
-def buildtreeslevelOrder(l=1,i=1,n=4):
-	if n==l:
-		return None
-	n=2**l-i
-	node=TreeNode(n)
-	node.left=buildtreeslevelOrder(l+1,2*i)
-	node.right=buildtreeslevelOrder(l+1,2*i-1)
-	return node
+	def is_empty(self):
+		return self.root == None
 
+	def preOrder(self,node):
+		if node == None:
+			return
+		# 先打印根结点，再打印左结点，后打印右结点
+		print(node.data)
+		self.preOrder(node.left)
+		self.preOrder(node.right)
 
-print(preorderTraversal(buildtreeslevelOrder()))
+	def inOrder(self,node):
+		if node == None:
+			return
+		# 先打印左结点，再打印根结点，后打印右结点
+		self.inOrder(node.left)
+		print(node.data)
+		self.inOrder(node.right)
 
+	def postOrder(self,node):
+		if node == None:
+			return
+		# 先打印左结点，再打印右结点，后打印根结点
+		self.postOrder(node.left)
+		self.postOrder(node.right)
+		print(node.data)
