921492785c6bc1029e7134a3f30603e9f58cd7a1
postpascal🦉 <apostpascal@gmail.com>
2019-11-02 16:42:23 +0800

    uodate

--- findUnsortedSubarray581.py
+++ findUnsortedSubarray581.py
@@ -15,3 +15,46 @@ class Solution(object):
             return 0
         else:
             return j-i+1
+
+
+
+
+
+# class Solution:
+#     def findUnsortedSubarray(self, nums: List[int]) -> int:
+#         l=len(nums)
+#         for i in range(1,len(nums)):
+#             if nums[i]<nums[i-1]:
+#                 l=i
+#                 break
+
+#         r=-1
+#         for i in range(len(nums)-2,-1,-1):
+#             if nums[i]>nums[i+1]:
+#                 r=i
+#                 break
+
+#         if r==-1: return 0
+#         if r<l:
+#             l,r=r,l
+#         ma=nums[l]
+#         mi=nums[l]
+#         for i in range(l,len(nums)):
+#             if nums[i]<mi:
+#                 mi=nums[i]
+#         for i in range(r+1):
+#             if nums[i]>ma:
+#                 ma=nums[i]
+#         li=l
+#         ri=r
+                
+#         for i in range(0,l+1):
+            
+#             if nums[i]>mi:
+#                 li=i
+#                 break
+#         for i in range(len(nums)-1,r-1,-1):
+#             if nums[i]<ma:
+#                 ri=i
+#                 break
+#         return ri-li+1
